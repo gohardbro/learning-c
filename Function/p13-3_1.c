1502894bd8550dfa9eada8ee148b4d187efe17b5
gohardbro <wkd9155@gmail.com>
2024-04-26 20:29:52 +0900

    add: p13-3_1.c

---
+++ Function/p13-3_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int average(int, int, int);
+int main() {
+  int score[5];
+
+  for (int i=0; i<5; i++) {
+    for (int j=0; j<3; j++) {
+      printf();
+    }
+  }
+  return 0;
+}
+
+int average(int score1, int score2, int score3) {
+  return (score1 + score2 + score3) / 3;
+}
